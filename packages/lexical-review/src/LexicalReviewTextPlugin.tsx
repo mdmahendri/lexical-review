7e759308b3233c3741a0bd4f6dceaba1bc6150aa
mdmahendri <mahendri.md@gmail.com>
2025-11-30 02:51:24 +0700

    add granularity into ReviewTextPlugin props

--- packages/lexical-review/src/LexicalReviewTextPlugin.tsx
+++ packages/lexical-review/src/LexicalReviewTextPlugin.tsx
@@ -5,11 +5,13 @@ import { registerReviewText } from ".";
 
 export function ReviewTextPlugin({
   contentEditable,
+  granularity = "character",  
 }: {
   contentEditable: JSX.Element;
+  granularity?: "word" | "character";
 }) {
   const [editor] = useLexicalComposerContext();
-  registerReviewText(editor);
+  registerReviewText(editor, granularity);
 
   return <>{contentEditable}</>;
 }
