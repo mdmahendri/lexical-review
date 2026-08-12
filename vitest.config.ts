527dfc7a21c952c0e773cf9bf978302ada107b65
mdmahendri <mahendri.md@gmail.com>
2026-08-12 10:27:24 +0700

    fix(test): prevent duplicate Vitest discovery

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    exclude: ['node_modules', 'dist', '.nx', '.idea', '.git', '.cache', '**/dist/**']
+    exclude: ['**/node_modules/**', 'dist', '.nx', '.idea', '.git', '.cache', '**/dist/**']
   },
 })
